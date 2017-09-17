a077e689d8d813b23a82b6605919e7437364d33d
xav2792 <xavier.engels@laposte.net>
2017-09-17 15:54:52 +0200

    utilisation du theme caos avec theme enfant

---
+++ wp-content/themes/caos-child/template-home.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: HomePage
+ */
+?>
+<?php get_header(); ?>
+
+<div class="container-fluid " >
+    ,fklejer
+    knverlrfk
+    ,vrekf,
+    </div>
+
+
+<?php get_footer(); ?>
