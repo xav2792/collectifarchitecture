16df315247eb10c904495f00136a074910de6390
xav2792 <xavier.engels@laposte.net>
2017-09-20 23:05:58 +0200

    ajout template des pages

---
+++ wp-content/themes/caos-child/template-3dModeling.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: Page
+Description: Template pour les pages
+ */
+
+get_template_part('template', '3dModeling');
+?>
--- wp-content/themes/caos-child/template-home.php
+++ wp-content/themes/caos-child/template-home.php
@@ -5,13 +5,13 @@ Template Name: Home
 ?>
 <?php get_header(); ?>
 
-<div class="container-fluid " style="overflow:hidden;">
+<div class="container-fluid">
 
     <?php
     $id_page = array(59,49,51,53,55,68);
     query_posts(array("post__in" => $id_page, 'post_type' => 'page', 'order' => 'ASC'));
-    echo "<div class='row ' >";
-    $i = 0;
+    echo "<div class='row'>";
+    $i = 1;
     while (have_posts()): the_post();
 
         echo "<div class='col-lg-3 col-md-4 col-sm-6 xol-xs-6 ' >";
@@ -19,13 +19,22 @@ Template Name: Home
         $imageData = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large');
         echo "<a href=" . esc_url(get_page_link($id_page[$i])) . ">";
         echo "<img class='img-responsive' src=" . $imageData[0] . ">";
-        echo "</a><div class='textimg'><a style='color:white ;text-decoration: none;' href=" . esc_url(get_page_link($id_page[$i])) . ">";
+        echo "</a><div class='textimg'><a href=" . esc_url(get_page_link($id_page[$i])) . ">";
         the_title();
         echo "</a></div></div></div>";
         $i++;
     endwhile;
     ?>
 </div>
+</div>
+
+<div class="bloc-services">
+<h2></h2>
+    <p></p>
+</div>
+
+<div class="bloc-realisations">
+
 </div>
 
 
